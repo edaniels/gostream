c35fb85da56537b38f39e6026410032fd2e29318
maximpertsov <maxim.pertsov@gmail.com>
2022-03-10 18:00:28 -0500

    Keep driver open until no video readers reference them (#6)

--- media/video.go
+++ media/video.go
@@ -2,11 +2,33 @@ package media
 
 import (
 	"context"
+	"fmt"
 	"image"
+	"sync"
 
 	"github.com/pion/mediadevices/pkg/driver"
 	"github.com/pion/mediadevices/pkg/io/video"
+	"go.viam.com/utils"
 )
+
+// ErrDriverInUse is returned when closing drivers that still being read from.
+type ErrDriverInUse struct {
+	label string
+}
+
+func (err *ErrDriverInUse) Error() string {
+	return fmt.Sprintf("driver is still in use: %s", err.label)
+}
+
+// driverRefManager is a lockable map of drivers and reference counts of video readers
+// that use them.
+type driverRefManager struct {
+	refs map[string]utils.RefCountedValue
+	mu   sync.Mutex
+}
+
+// initialize a global driverRefManager
+var driverRefs = driverRefManager{refs: map[string]utils.RefCountedValue{}}
 
 // A VideoReadCloser is a video.Reader that requires it be closed.
 type VideoReadCloser interface {
@@ -24,6 +46,23 @@ type videoReadCloser struct {
 	videoReader video.Reader
 }
 
+// NewVideoReadCloser instantiates a new video read closer and references the given
+// driver.
+func NewVideoReadCloser(d driver.Driver, r video.Reader) *videoReadCloser {
+	driverRefs.mu.Lock()
+	defer driverRefs.mu.Unlock()
+
+	label := d.Info().Label
+	if rcv, ok := driverRefs.refs[label]; ok {
+		rcv.Ref()
+	} else {
+		driverRefs.refs[label] = utils.NewRefCountedValue(d)
+		driverRefs.refs[label].Ref()
+	}
+
+	return &videoReadCloser{d, r}
+}
+
 func (vrc videoReadCloser) Read() (image.Image, func(), error) {
 	return vrc.videoReader.Read()
 }
@@ -33,5 +72,20 @@ func (vrc videoReadCloser) Next(ctx context.Context) (image.Image, func(), error
 }
 
 func (vrc videoReadCloser) Close() error {
-	return vrc.videoDriver.Close()
+	driverRefs.mu.Lock()
+	defer driverRefs.mu.Unlock()
+
+	label := vrc.videoDriver.Info().Label
+	if rcv, ok := driverRefs.refs[label]; ok {
+		if rcv.Deref() {
+			delete(driverRefs.refs, label)
+			return vrc.videoDriver.Close()
+		}
+	} else {
+		return vrc.videoDriver.Close()
+	}
+
+	// Do not close if a driver is being referenced. Client will decide what to do if
+	// they encounter this error.
+	return &ErrDriverInUse{label}
 }
