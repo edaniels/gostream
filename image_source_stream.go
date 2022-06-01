0ebcb567f5d3fca0dba58967a159e9b5b91b822d
Gautham Varadarajan <gautham@viam.com>
2022-06-01 12:53:46 -0400

    introduce exponential backoff when receiving consecutive frame errors

--- image_source_stream.go
+++ image_source_stream.go
@@ -2,7 +2,21 @@ package gostream
 
 import (
 	"context"
+	"errors"
+	"math"
+	"time"
 )
+
+// maxErrorSleepSec sets a maximum sleep time to the exponential backoff
+// determined by sleepTimeFromErrorCount
+var maxErrorSleepSec = math.Pow10(9) * 2 // two seconds
+const maxSleepAttempts = 20
+
+func sleepTimeFromErrorCount(errCount int) int {
+	expBackoffMillisec := math.Pow(6.0, float64(errCount))
+	expBackoffNanosec := expBackoffMillisec * math.Pow10(6)
+	return int(math.Min(expBackoffNanosec, maxErrorSleepSec))
+}
 
 // streamSource will stream a source of images forever to the stream until the given context tells it to cancel.
 func streamSource(ctx context.Context, once func(), is ImageSource, stream Stream) {
@@ -14,6 +28,8 @@ func streamSource(ctx context.Context, once func(), is ImageSource, stream Strea
 		return
 	case <-stream.StreamingReady():
 	}
+	var prevErr error
+	errorCount := 0
 	for {
 		select {
 		case <-ctx.Done():
@@ -23,7 +39,21 @@ func streamSource(ctx context.Context, once func(), is ImageSource, stream Strea
 		frame, release, err := is.Next(ctx)
 		if err != nil {
 			Logger.Debugw("error getting frame", "error", err)
+			if prevErr == nil {
+				prevErr = err
+			} else if errors.Is(prevErr, err) {
+				errorCount += 1
+			} else {
+				errorCount = 0
+			}
+			if errorCount > 0 {
+				errorCount = int(math.Min(float64(errorCount), float64(maxSleepAttempts)))
+				dur := sleepTimeFromErrorCount(errorCount)
+				time.Sleep(time.Duration(dur))
+			}
 			continue
+		} else {
+			errorCount = 0
 		}
 		select {
 		case <-ctx.Done():
