ba6d560975565fe4b8406ae56fce98900a8b65a4
Gautham Varadarajan <gauthamvaradarajan@gmail.com>
2022-06-16 17:53:14 -0400

    add support for error handling in streamSource (#10)
    Co-authored-by: Gautham Varadarajan <gautham@viam.com> (AKA GoMath)

--- image_source_stream.go
+++ image_source_stream.go
@@ -4,8 +4,14 @@ import (
 	"context"
 )
 
+// ErrorHandler receives the error returned by ImageSource.Next
+// regardless of whether or not the error is nil (This allows
+// for error handling logic based on consecutively retrieved errors).
+// It returns a boolean indicating whether or not the loop should continue.
+type ErrorHandler func(ctx context.Context, frameErr error) bool
+
 // streamSource will stream a source of images forever to the stream until the given context tells it to cancel.
-func streamSource(ctx context.Context, once func(), is ImageSource, stream Stream) {
+func streamSource(ctx context.Context, once func(), is ImageSource, stream Stream, errHandler ErrorHandler) {
 	if once != nil {
 		once()
 	}
@@ -21,8 +27,9 @@ func streamSource(ctx context.Context, once func(), is ImageSource, stream Strea
 		default:
 		}
 		frame, release, err := is.Next(ctx)
-		if err != nil {
-			Logger.Debugw("error getting frame", "error", err)
+		// if errHandler returns true, it means DO NOT continue with the
+		// the rest of the logic on the current iteration
+		if errHandler(ctx, err) {
 			continue
 		}
 		select {
@@ -35,5 +42,19 @@ func streamSource(ctx context.Context, once func(), is ImageSource, stream Strea
 
 // StreamSource streams the given image source to the stream forever until context signals cancellation.
 func StreamSource(ctx context.Context, is ImageSource, stream Stream) {
-	streamSource(ctx, nil, is, stream)
+	streamSource(ctx, nil, is, stream, func(ctx context.Context, frameErr error) bool {
+		if frameErr != nil {
+			Logger.Debugw("error getting frame", "error", frameErr)
+			return true
+		}
+		return false
+	})
+}
+
+// StreamSourceWithErrorHandler streams the given image source to the stream forever
+// until context signals cancellation, frame errors are sent via the error handler
+func StreamSourceWithErrorHandler(
+	ctx context.Context, is ImageSource, stream Stream, errHandler ErrorHandler,
+) {
+	streamSource(ctx, nil, is, stream, errHandler)
 }
