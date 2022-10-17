6324dec605ffcaf3c963678f210ec77ced9aac62
Clyde Bazile <clyde@viam.com>
2022-10-17 13:37:51 -0400

    unexport

--- media_test.go
+++ media_test.go
@@ -12,13 +12,13 @@ import (
 	"go.viam.com/test"
 )
 
-type ImageSource struct {
+type imageSource struct {
 	Images []image.Image
 	idx    int
 }
 
 // Returns the next image or nil if there are no more images left. This should never error.
-func (is *ImageSource) Read(_ context.Context) (image.Image, func(), error) {
+func (is *imageSource) Read(_ context.Context) (image.Image, func(), error) {
 	if is.idx >= len(is.Images) {
 		return nil, func() {}, nil
 	}
@@ -27,12 +27,11 @@ func (is *ImageSource) Read(_ context.Context) (image.Image, func(), error) {
 	return img, func() {}, nil
 }
 
-// Nothing to do.
-func (is *ImageSource) Close(_ context.Context) error {
+func (is *imageSource) Close(_ context.Context) error {
 	return nil
 }
 
-func PNGtoImage(t *testing.T, path string) image.Image {
+func pngToImage(t *testing.T, path string) image.Image {
 	t.Helper()
 	openBytes, err := os.ReadFile(path)
 	test.That(t, err, test.ShouldBeNil)
@@ -43,15 +42,15 @@ func PNGtoImage(t *testing.T, path string) image.Image {
 
 func TestReadMedia(t *testing.T) {
 	colors := []image.Image{
-		PNGtoImage(t, "data/red.png"),
-		PNGtoImage(t, "data/blue.png"),
-		PNGtoImage(t, "data/green.png"),
-		PNGtoImage(t, "data/yellow.png"),
-		PNGtoImage(t, "data/fuchsia.png"),
-		PNGtoImage(t, "data/cyan.png"),
+		pngToImage(t, "data/red.png"),
+		pngToImage(t, "data/blue.png"),
+		pngToImage(t, "data/green.png"),
+		pngToImage(t, "data/yellow.png"),
+		pngToImage(t, "data/fuchsia.png"),
+		pngToImage(t, "data/cyan.png"),
 	}
 
-	var imgSource ImageSource
+	var imgSource imageSource
 	// Create a deep copy
 	imgSource.Images = append(imgSource.Images, colors...)
 
@@ -64,10 +63,10 @@ func TestReadMedia(t *testing.T) {
 	}
 
 	// Test image comparison can fail if two images are not the same
-	imgSource.Images = []image.Image{PNGtoImage(t, "data/red.png")}
+	imgSource.Images = []image.Image{pngToImage(t, "data/red.png")}
 	videoSrc = NewVideoSource(&imgSource, prop.Video{})
 
-	blue := PNGtoImage(t, "data/blue.png")
+	blue := pngToImage(t, "data/blue.png")
 	red, _, err := ReadMedia(context.Background(), videoSrc)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, red, test.ShouldNotEqual, blue)
