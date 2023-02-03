122b7d86ca6ce16975bc63e7b56730e62295151b
Eric Daniels <eric@erdaniels.com>
2023-02-03 11:29:38 -0500

    ci work

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-os="$(uname -s)"
-if [[ "$os" == "Darwin" ]]; then
-	args=$(go list -f '{{.Dir}}' ./... | grep -v mmal)
-else
-	args="./..."
-fi
+args=$(go list -f '{{.Dir}}' ./... | grep -v mmal)
 set -euo pipefail
 go test -tags=no_skip -race $args -json -v 2>&1 | gotestfmt
